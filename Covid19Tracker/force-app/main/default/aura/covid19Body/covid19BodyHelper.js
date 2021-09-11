bc68c6d9dbc82d1264f57baa8824bc90c70dd1db
nareshkchoudhary <naresh.kumar@mirketa.com>
2021-09-11 14:32:36 +0530

    Fixed Search Bug

--- Covid19Tracker/force-app/main/default/aura/covid19Body/covid19BodyHelper.js
+++ Covid19Tracker/force-app/main/default/aura/covid19Body/covid19BodyHelper.js
@@ -6,10 +6,10 @@
             var state=response.getState();
             if(state==="SUCCESS"||state=="DRAFT"){
                 var res=response.getReturnValue();
-                console.log(JSON.stringify(res));
+               // console.log(JSON.stringify(res));
 
                 //total stats
-                console.log('tt'+res.total);
+                //console.log('tt'+res.total);
                 component.set("v.totalConfirmed",res.total.TotalConfirmed);
                 component.set("v.newConfirmed",res.total.NewConfirmed);
                 component.set("v.totalConfirmed",res.total.TotalConfirmed);
@@ -17,12 +17,17 @@
                 component.set("v.totalRecovered",res.total.TotalRecovered);
 
                 var totalRecords=res.countries.length;
-                var pageSize=20;
+                var pageSize=component.get("v.pageSize");
+                var pageNumber=component.get("v.pageNumber");
+                var offset=(pageNumber-1)*pageSize;
+                var startRecord=offset+1;
+                var endRecord=totalRecords>=(pageNumber*pageSize)?(pageNumber*pageSize):totalRecords;
                 var totalPages=Math.ceil(totalRecords/pageSize);
+                console.log("start:"+startRecord+",end:"+endRecord);
                 //for country specific
                 var dataArray=[];
-                for(var i=0;i<20;i++){
-                    if(query==="byCountry" && countryName!='' && res.countries[i].Country.toLowerCase().includes(countryName)){
+                for(var i=startRecord;i<endRecord;i++){
+                    if(query==="byCountry" && countryName!='' && res.countries[i].Country.toLowerCase().includes(countryName.toLowerCase())){
                          var data={
                         id:i,
                         country:res.countries[i].Country,
@@ -54,7 +59,7 @@
                 //console.log("in js:"+data);
                 if(dataArray.length!=0 || dataArray!=null){
                     component.set("v.data",dataArray);
-                    component.set("v.pageNumber",1);
+                    component.set("v.pageNumber",pageNumber);
                     component.set("v.totalPages",totalPages);
                     component.set("v.totalRecords",res.countries.length);
                 }
